2fc8872fd3b027b8e9f2eefbf48df75d7cdc2031
Chadwicksracing <42011043+Chadwicksracing@users.noreply.github.com>
2022-05-25 15:11:32 -0400

    xz tar cmd line updated

--- install.sh
+++ install.sh
@@ -184,7 +184,7 @@ function bootstrap() {
     fi
   elif [[ ! -z $BOOTSTRAP_ANS ]]; then
     echo -e "${YELLOW}Downloading wallet bootstrap please be patient...${NC}"
-    curl -L $BOOTSTRAP_TAR | tar xz -C $HOME/$CONFIG_DIR
+    curl -L $BOOTSTRAP_TAR | tar xf -C $HOME/$CONFIG_DIR # for xz archives the command needs to be xf
   else
     echo -e "${YELLOW}Skipping bootstrap...${NC}"
   fi
